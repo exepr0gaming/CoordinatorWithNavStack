0e15b3e565daa48f6ecbb4386c18cdd3d8427a0c
exepr0gaming <exehomework@gmail.com>
2023-07-10 19:24:31 +0300

    Initial Commit

---
+++ CoordinatorWithNavStack/CoordinatorWithNavStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorWithNavStackApp.swift
+//  CoordinatorWithNavStack
+//
+//  Created by Admin on 10.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorWithNavStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
